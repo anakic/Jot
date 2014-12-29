b8de89f61dbddd3f664ee2630bd3ce809ec13d81
Antonio (kuci) <antonio.nakic@gmail.com>
2014-12-29 20:31:14 +0100

    signing

--- Thingie.Tracking/Properties/AssemblyInfo.cs
+++ Thingie.Tracking/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Thingie.Tracking.UnitTests")]
+//[assembly: InternalsVisibleTo("Thingie.Tracking.UnitTests")]
