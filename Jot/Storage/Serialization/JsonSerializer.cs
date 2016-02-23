249391a574689aafe50ad057c205433c0fc4d64f
antonio laptio <antonio.nakic@gmail.com>
2016-02-23 17:59:20 +0100

    Using Newtonsoft JSON.NET for serialization again, it's just a better serializer all around then the built in JavascriptSerializer.

--- Jot/Storage/Serialization/JsonSerializer.cs
+++ Jot/Storage/Serialization/JsonSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,14 @@ namespace Jot.Storage.Serialization
 {
     public class JsonSerializer : ISerializer
     {
-        JavaScriptSerializer _serializer = new JavaScriptSerializer();
-
         public virtual string Serialize(object obj)
         {
-            return _serializer.Serialize(obj);
+            return JsonConvert.SerializeObject(obj);
         }
 
         public virtual object Deserialize(string serialized, Type originalType)
         {
-            return _serializer.Deserialize(serialized, originalType);
+            return JsonConvert.DeserializeObject(serialized, originalType);
         }
     }
 }
