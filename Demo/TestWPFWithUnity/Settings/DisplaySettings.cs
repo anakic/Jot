49c97d7022cfb1c9123c8d8d5deb84c5cfce7b25
Mike Eason <mike.eason@mail.com>
2016-05-08 13:44:21 +0100

    Added a nicer way of invoking property changed, this method does not need to pass in the name of the property.

--- Demo/TestWPFWithUnity/Settings/DisplaySettings.cs
+++ Demo/TestWPFWithUnity/Settings/DisplaySettings.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 using System.Windows.Media;
+using System.Runtime.CompilerServices;
 
 namespace TestWPFWithUnity.Settings
 {
@@ -17,20 +18,20 @@ namespace TestWPFWithUnity.Settings
         public FontFamily Font
         {
             get { return _font; }
-            set { _font = value; OnPropertyChanged("Font"); }
+            set { _font = value; OnPropertyChanged(); }
         }
 
         private decimal _fontSize = 15;
         public decimal FontSize
         {
             get { return _fontSize; }
-            set { _fontSize = value; OnPropertyChanged("FontSize"); }
+            set { _fontSize = value; OnPropertyChanged(); }
         }
 
         [field: NonSerialized]
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string propertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
             var handler = PropertyChanged;
             if (handler != null)
